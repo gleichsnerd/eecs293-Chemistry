5afe1eb71c75bf6ca052e29c823099fcbca70079
Adam Gleichsner <amg188@case.edu>
2014-11-04 20:00:42 -0500

    Finished the code. Whoops

---
+++ src/Chemistry.java
@@ -0,0 +1,184 @@
+import java.util.regex.MatchResult;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Chemistry - Programming Assignment 10
+ * @author Adam Gleichsner
+ * Static class that checks a string for valid formula syntax,
+ * but not valid chemistry.
+ */
+public class Chemistry {
+
+	/* Let's take a look at these regex patterns
+	 * When looking at a piece of a chemical formula, it fits into one of two cases:
+	 * 
+	 * Case 1 - Non-parenthesized
+	 * In this case, we're only checking for valid symbols (One capital, up to 2 lowercase)
+	 * and element count (none or 2 and higher, since just 0 or 1 is pointless). For this, we fit the
+	 * pattern of:
+	 * [A-Z][a-z]{0,2}([2-9]|[1-9]+[0-9]+)*
+	 * 
+	 * Case 2 - Parenthesized
+	 * Here, we can have repeated occurrences of case 1 contained within parentheses. In order
+	 * for parentheses to be valid, they must have a count at the end (otherwise why have them?).
+	 * In order to handle nested parentheses, we can't simply use regex. Instead, we remove them from
+	 * the formula by first finding all generic cases of single parenthesis use (i.e. (sometextinhere))
+	 * and then checking if it fits the valid pattern:
+	 * \(([A-Z][a-z]{0,2}([2-9]|[1-9]+[0-9]+)*)+\)([2-9]|[1-9]+[0-9]+)+
+	 * 
+	 * If it matches, then we remove it. If not, then it's invalid and we should indicate that.
+	 * 
+	 * We check the final string (if it's not null) against the simple pattern by appending \A and 
+	 * \Z to the ends, effectively binding the pattern to the entire string so we don't have any
+	 * loose characters that may be incorrect. In the case of a formula that is entirely parenthesized,
+	 * the return will be an empty string, so we also check for that.
+	 * 
+	 * We make the regex strings private and final so they can't be accessed or changed at any point.
+	 */
+	private static final String CORRECT_SIMPLE_SYNTAX_PATTERN = "\\A([A-Z][a-z]{0,2}([2-9]|[1-9]+[0-9]+)*)+\\Z";
+	private static final String GENERIC_PARENTHESES_PATTERN = "\\(([A-z]*[0-9]*)+\\)\\d*";
+	private static final String CORRECT_PARENTHESES_PATTERN = "\\(([A-Z][a-z]{0,2}([2-9]|[1-9]+[0-9]+)*)+\\)([2-9]|[1-9]+[0-9]+)+";
+	
+	/**
+	 * Public front of the checking process. Takes any String input and will
+	 * return "T" if valid and "F" if now. Mostly here in case the specs
+	 * actually want T & F instead of boolean true and false
+	 * @param formula - input string formula to check syntax of
+	 * @return String - "T" if valid, "F" otherwise
+	 */
+	public static String checkSyntax(String formula) {
+		throwIfInputIsNull(formula);
+		//Declare as final for security, ya know
+		final String TRUE_OUTPUT = "T";
+		final String FALSE_OUTPUT = "F";
+		boolean isValid = Chemistry.isSyntaxCorrect(formula);
+		
+		if (isValid)
+			return TRUE_OUTPUT;
+		else
+			return FALSE_OUTPUT;
+	}
+	
+	/**
+	 * Public method that actually does the checking against the defined regex
+	 * patter. Returns true if syntactically valid, false otherwise
+	 * @param formula - input string to check syntax of
+	 * @return boolean - true if syntactically valid, false otherwise
+	 */
+	public static boolean isSyntaxCorrect(String formula) {	
+		throwIfInputIsNull(formula);
+		throwIfInvalidString(formula);
+		
+		//We need to remove parentheses in case they're nested
+		if (containsParentheses(formula))
+			formula = removeParentheses(formula);
+		
+		//If our parentheses statements weren't invalid and we either have correct remaining formula
+		//or we've already processed everything
+		if (formula != null && (formula.matches(CORRECT_SIMPLE_SYNTAX_PATTERN) || formula.equals("")))
+			return true;
+		else
+			return false;
+	}
+	
+	//Private methods
+	
+	/**
+	 * Private helper to check if a formula contains parentheses to remove.
+	 * Compares the formula against a generic pattern to grab both
+	 * correct and incorrect cases.
+	 * @param formula - string to search
+	 * @return boolean - true if parenthses found, false otherwise
+	 */
+	private static boolean containsParentheses (String formula) {
+		
+		//Create a pattern and matcher to check the string
+		Pattern p = Pattern.compile(GENERIC_PARENTHESES_PATTERN);
+		Matcher m = p.matcher(formula);
+
+		//If we find any parentheses, then we need to take them out
+		if(m.find()) 
+			return true;
+		return false;
+	}
+	
+	/**
+	 * Private helper that loops through the formula and removes all valid
+	 * parentheses.
+	 * @param formula - string to remove parenthesis statements from
+	 * @return String - string without valid parenthesis statements, null if invalid statement found
+	 */
+	private static String removeParentheses (String formula) {
+		//Create pattern and matcher to find all parenthesis cases, correct and incorrect
+		Pattern p = Pattern.compile(GENERIC_PARENTHESES_PATTERN);
+		Matcher m = p.matcher(formula);
+		
+		//While there is still a generic parenthesis case
+		while (m.find()) {
+			//If it's correct, remove it
+			if(m.group().matches(CORRECT_PARENTHESES_PATTERN))
+				formula = formula.replace(m.group(), "");
+			else
+				return null;
+			//Reset the matcher with the newly edited string
+			m = p.matcher(formula);
+		}
+		return formula;
+	}
+	
+	/**
+	 * Private helper that checks if formula is a valid string with
+	 * characters and no whitespace, throws exception if invalid
+	 * @param formula - input string to check if valid string
+	 * @throws IllegalArgumentException
+	 */
+	private static void throwIfInvalidString(String formula) throws IllegalArgumentException{
+		throwIfInputIsNull(formula);
+		
+		if (formula.isEmpty())
+			throw new IllegalArgumentException("Error: input is empty string");
+		else {
+			//Check each character for whitespace since we have no way
+			//to infer what the intention of said whitespace was
+			for (char c: formula.toCharArray()) {
+				if (Character.isWhitespace(c))
+					throw new IllegalArgumentException("Error: input contains illegal whitespace");
+			}
+		}
+	}
+	
+	/**
+	 * Private helper that will throw exception if any input is a null object
+	 * @param objects - objects to check
+	 * @throws IllegalArgumentException
+	 */
+	private static void throwIfInputIsNull(Object... objects) throws IllegalArgumentException {
+		for (Object obj: objects) {
+			if (obj == null)
+				throw new IllegalArgumentException("Error: an argument is null");
+		}
+	}
+
+	/**
+	 * Main method can take multiple strings as input and check each one
+	 * for valid syntax. Will throw exception if no arguments are given.
+	 * @param args - Array of strings to check
+	 * @throws IllegalArgumentException
+	 */
+	public static void main(String[] args) throws IllegalArgumentException {
+		throwIfInputIsNull((Object[])args);
+		
+		String checkedOut = " ";
+		if (args.length == 0)
+			throw new IllegalArgumentException("Error: invalid number of arguments");
+		else {
+			for(String formula: args) {
+				
+				checkedOut = Chemistry.checkSyntax(formula);
+				System.out.print(formula + " : " + checkedOut);
+			}
+		}
+	}
+
+}
---
+++ src/ChemistryTest.java
@@ -0,0 +1,85 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class ChemistryTest {
+
+
+	@Test
+	public void testSingleElement(){
+		assertTrue(Chemistry.isSyntaxCorrect("O"));
+		assertTrue(Chemistry.isSyntaxCorrect("Br"));
+		assertTrue(Chemistry.isSyntaxCorrect("Umm"));
+		
+		assertFalse(Chemistry.isSyntaxCorrect("o"));
+		assertFalse(Chemistry.isSyntaxCorrect("Uhhhhhhhhhh"));
+		
+	}
+
+	@Test
+	public void testManyElements(){
+		assertTrue(Chemistry.isSyntaxCorrect("OH"));
+		assertTrue(Chemistry.isSyntaxCorrect("BrH"));
+		assertTrue(Chemistry.isSyntaxCorrect("BrHe"));
+		assertTrue(Chemistry.isSyntaxCorrect("UmmOhh"));
+		
+		assertFalse(Chemistry.isSyntaxCorrect("ErrrrrUhh"));
+		assertFalse(Chemistry.isSyntaxCorrect("UmmUhhhhhhhhh"));
+	}
+
+	@Test
+	public void testMultiples(){
+		assertTrue(Chemistry.isSyntaxCorrect("O2"));
+		assertTrue(Chemistry.isSyntaxCorrect("H2O"));
+		assertTrue(Chemistry.isSyntaxCorrect("H2O2"));
+		assertTrue(Chemistry.isSyntaxCorrect("Hi2Ho4"));
+		
+		assertFalse(Chemistry.isSyntaxCorrect("o5"));
+		assertFalse(Chemistry.isSyntaxCorrect("O1"));
+		assertFalse(Chemistry.isSyntaxCorrect("O0"));
+		assertFalse(Chemistry.isSyntaxCorrect("H1H8"));
+		assertFalse(Chemistry.isSyntaxCorrect("L8RH8R0"));
+	}
+
+
+	@Test
+	public void testParentheses(){
+		assertTrue(Chemistry.isSyntaxCorrect("(NaCl)2"));
+		assertTrue(Chemistry.isSyntaxCorrect("(Na2Cl)4"));
+		assertTrue(Chemistry.isSyntaxCorrect("(Na2Cl4)6"));
+		
+		assertFalse(Chemistry.isSyntaxCorrect("(NaCl"));
+		assertFalse(Chemistry.isSyntaxCorrect("Boo)"));
+		assertFalse(Chemistry.isSyntaxCorrect("(H2O)"));
+	}
+
+	@Test
+	public void testNestedParentheses(){
+		assertTrue(Chemistry.isSyntaxCorrect("((OH2)3Pr)4"));
+		assertTrue(Chemistry.isSyntaxCorrect("((OH2)3(Hgg)4Pr)4"));
+		
+		assertFalse(Chemistry.isSyntaxCorrect("(((((((((((((WhOoPs)"));
+		assertFalse(Chemistry.isSyntaxCorrect("((NaCl)(Oh)2)2"));
+	}
+
+	@Test
+	public void testInvalidinput(){
+		try{
+			Chemistry.isSyntaxCorrect(null);
+			fail("Exception should be thrown on null input");
+		} catch(IllegalArgumentException e) {};
+		
+		try{
+			Chemistry.isSyntaxCorrect("");
+			fail("Exception should be thrown on empty string input");
+		} catch(IllegalArgumentException e) {};
+		
+		try{
+			Chemistry.isSyntaxCorrect("Hi ThErE1");
+			fail("Exception should be thrown on whitespace in input");
+		} catch(IllegalArgumentException e) {};
+	}
+
+
+}
